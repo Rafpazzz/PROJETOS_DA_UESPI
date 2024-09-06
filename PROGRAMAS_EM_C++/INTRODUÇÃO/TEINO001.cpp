5757645c5dcba8f71935c525d487d94255d72200
Rafpazzz <rafael_mourapaz@hotmail.com>
2024-09-06 19:59:59 -0300

    lsa[

--- PROGRAMAS_EM_C++/INTRODUÇÃO/TEINO001.cpp
+++ PROGRAMAS_EM_C++/INTRODUÇÃO/TEINO001.cpp
@@ -5,18 +5,26 @@ using namespace std;
 int main(){
     int vet[] = {1, 2, -3, 4, -5, 6, -7};
     const int n = 7;
-    int idx = -1;
+    int pri , ult;
+    int c = 0;
 
     for (int i = 0; i < n; i++)
     {
         if (vet[i]<0)
         {
-            idx = i;
-            cout << " "<<idx;
+            if (c == 0)
+            {
+            c++;
+            pri = i;
+            ult = pri;
+            }else{
+            ult = i;
+            
+             }
         }
          
     }
 
-   
+   cout << "primeiro: "<< pri<< " ultimo: "<<ult;
     
 }
