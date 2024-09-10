da8cd305196aa6fc6a71a581cb5199f6fbaccf74
Rafpazzz <rafael_mourapaz@hotmail.com>
2024-09-10 16:33:56 -0300

    asef

---
+++ PROGRAMAS_EM_C++/INTRODUÇÃO/TREINO002.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    double largura, altura, area;
+
+    cout <<"Informe a largura: ";
+    cin >> largura;
+    cout <<"Informe a altura: ";
+    cin>> altura;
+
+    area = altura *largura;
+
+    cout <<"Area: "<< area;
+
+    return 0;
+}
