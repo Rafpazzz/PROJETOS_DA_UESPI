caa4b166240f17a2544386ab6e1ea7713e18f1df
Rafpazzz <rafael_mourapaz@hotmail.com>
2024-09-13 14:46:51 -0300

    iniciando o jogo da forca

---
+++ PROGRAMAS_EM_C++/INTRODUÇÃO/AULA013.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+int main(){
+
+    char palavra[30], letra[1], secreta[30];
+    int tam, i, chances, acertos; 
+    bool acerto;
+
+    chances=0;
+    tam=0;
+    acerto = false;
+    acertos=0;
+
+    cout<<"Digite a palavra secreta: ";
+    cin>> palavra;
+    system("cls");
+
+    while (palavra[i] != '\0'){
+        i++;
+        tam++;
+    }
+
+    for ( i = 0; i < 30; i++){
+        secreta[i]='-';
+    }
+    
+    
+
+    return 0;
+}
