54264ec21ea7124adcd991fc4ef570e00167a66d
Rafpazzz <rafael_mourapaz@hotmail.com>
2024-09-16 10:06:19 -0300

    jogo da velha em C++

--- PROGRAMAS_EM_C++/INTRODUÇÃO/AULA013.cpp
+++ PROGRAMAS_EM_C++/INTRODUÇÃO/AULA013.cpp
@@ -9,7 +9,7 @@ int main(){
     int tam, i, chances, acertos; 
     bool acerto;
 
-    chances=0;
+    chances=6;
     tam=0;
     acerto = false;
     acertos=0;
@@ -27,7 +27,33 @@ int main(){
         secreta[i]='-';
     }
     
+    while ((chances > 0)&&(acertos < tam)){
+        cout<<"Chances restantes: "<<chances<<endl<< endl;
+        cout<<"Palavra secreta: ";
+        for ( i = 0; i < tam; i++){
+            cout<<secreta[i];
+        }
+        cout<<"\n\nDigite uma letra: ";
+        cin>> letra[0];
+        for(i = 0; i<tam;i++){
+            if(palavra[i]==letra[0]){
+                acerto = true;
+                secreta[i]=palavra[i];
+                acertos++;
+            }
+        }
+        if (!acerto){
+            chances--;
+        }
+        acerto=false;
+        system("cls");
+    }
     
+    if(acertos==tam){
+        cout<<"Voce venceu!";
+    }else{
+        cout<<"Voce perdeu!";
+    }
 
     return 0;
 }
