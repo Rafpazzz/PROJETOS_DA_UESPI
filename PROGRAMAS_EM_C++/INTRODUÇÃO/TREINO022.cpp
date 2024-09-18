36ea273bedeabc3ab668287ccf1f6844352d3ec9
Rafpazzz <rafael_mourapaz@hotmail.com>
2024-09-18 16:45:49 -0300

    commit

--- PROGRAMAS_EM_C++/INTRODUÇÃO/TREINO022.cpp
+++ PROGRAMAS_EM_C++/INTRODUÇÃO/TREINO022.cpp
@@ -19,7 +19,10 @@ int multiplicacao(int num1, int num2){
 }
 
 int divisao(int num1, int num2){
-
+    if(num2 == 0){
+        cout<<"O peração invalida.";
+        break;
+    }
     return num1 / num2;
 }
 
@@ -29,6 +32,28 @@ int operacao(int num1, int num2, funcpont pont){
 
 int main(){
 
-    
+    char escolha;
+    int num1 = 7;
+    int num2 = 4;
+
+    cout<<"Escolha: ";
+    cin>>escolha;
+
+    switch(escolha){
+        case '+':
+            cout<< operacao(num1, num2, soma);
+            break;
+
+        case '-':
+            cout<< operacao(num1, num2, subitracao);
+            break;
+
+        case '*':
+            cout<< operacao(num1, num2, multiplicacao);
+            break;
+        
+        case '/':
+            cout<< operacao(num1, num2, subitracao);
+    }
 
 }
