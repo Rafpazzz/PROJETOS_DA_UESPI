273459c0e306c9e0ed6c930951ee3c6533d8aba0
Rafpazzz <rafael_mourapaz@hotmail.com>
2024-09-28 11:21:17 -0300

    implementando novas funçoes

--- PROGRAMAS_EM_C++/INTRODUÇÃO/TREINO044.cpp
+++ PROGRAMAS_EM_C++/INTRODUÇÃO/TREINO044.cpp
@@ -36,59 +36,99 @@ class listaDupEnc1{
             }
         }
 
-       void insereNo(int num, int idx){
-            No* busca = primNo;
-            No* insere = new No();
-            insere->valor = num;
 
-            if(primNo == nullptr && ultiNo == nullptr){
-                primNo = insere;
-                ultiNo = insere;
+        void tamanhoLista(){
+            No* conta = primNo;
+            int n = 0;
+
+            if(primNo == nullptr){
+                std::cout<<"Lista vazia.";
                 return;
             }
 
-            while(busca != nullptr && busca->valor != idx){
-                busca = busca->prox;
+            while(conta != nullptr){
+                conta = conta->prox;
+                n++;
             }
 
-           if(busca == nullptr){
-                std::cout<<"Posicao invalida.";
-                delete insere;
-                return;
-           }
-           
-           insere->prox = busca;
-           insere->ant = busca->ant;
-           
-           if(busca->ant != nullptr){
-                busca->ant->prox = insere;
-           }else{
-                primNo = insere;
-           }
+            std::cout<<"A lista possui "<<n<<" elementos.";
+        }
 
-           busca->ant = insere;
-           
-       }
+        void listaVazia(){
+            No* verifica = primNo;
+
+            if(primNo == nullptr){
+                std::cout<<"Lista nao possui elementos.";
+            }
+        }
+
+        void invereteLista(){
+            No* percorre = ultiNo;
+
+            while(percorre != primNo){
+                std::cout<<percorre->valor<<std::endl;
+                percorre = percorre->ant;
+            }
+            std::cout<<primNo->valor;
+        }
+
+        No* getPrimeiroNo(){
+            return primNo;
+        }
+
+        No* getUltimoNo(){
+            return ultiNo;
+        }
+
+        void concatenaLista(listaDupEnc1& list1, listaDupEnc1& list2){
+            No* junta = list1.getUltimoNo();
+
+            if(junta == nullptr){
+                std::cout<<"Lista vazia.";
+                junta = list2.getPrimeiroNo();
+            }if(junta == nullptr){
+                std::cout<<"As duas listas estao vazias.";
+                return;
+            }
+
+            junta->prox = list2.getPrimeiroNo();
+            list2.getPrimeiroNo()->ant = junta;
+
+            No* imprimiTudo = list1.getPrimeiroNo();
+
+            while(imprimiTudo != nullptr){
+                std::cout<<imprimiTudo->valor<<std::endl;
+                imprimiTudo = imprimiTudo->prox;
+            }
+        }
+       
 
         void imprimir(){
-            No* percorre = primNo;
+            No* percorre = ultiNo;
 
             while(percorre != nullptr){
                 std::cout<<percorre->valor<<std::endl;
-                percorre = percorre->prox;
+                percorre = percorre->ant;
             }
         }
 };
 
 int main(){
-    listaDupEnc1 numero;
+    listaDupEnc1 lista1;
+    listaDupEnc1 lista2;
+    listaDupEnc1 junta;
 
-    numero.inserirFinal(10);
-    numero.inserirFinal(20);
-    numero.inserirFinal(30);
-    numero.inserirFinal(40);
+    lista1.inserirFinal(10);
+    lista1.inserirFinal(20);
+    lista1.inserirFinal(30);
+    lista1.inserirFinal(40);
 
-    numero.insereNo(50, 20);
+    lista2.inserirFinal(50);
+    lista2.inserirFinal(60);
+    lista2.inserirFinal(70);
+    lista2.inserirFinal(80);
 
-    numero.imprimir();
+    junta.concatenaLista(lista1, lista2);
+
+    
 }
