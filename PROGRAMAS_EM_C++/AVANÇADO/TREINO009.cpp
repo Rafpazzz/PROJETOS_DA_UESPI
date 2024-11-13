6dbab28ed2cdb70b1bcb079a475f91f98728c591
Rafael Paz <rafael_mourapaz@hotmail.com>
2024-11-13 18:58:35 -0300

    contruindo fila encadeada novamente

--- PROGRAMAS_EM_C++/AVANÇADO/TREINO009.cpp
+++ PROGRAMAS_EM_C++/AVANÇADO/TREINO009.cpp
@@ -1,55 +1,82 @@
-//TREINO PARA A PROVA DE ESTRUTURA DE DADOS QUE OCORRE NO DIA 08/11/2024
-
 #include <iostream>
 
-constexpr int MAX = 100;
+class Node {
+    public:
+        Node *next;
+        int value;
+};
 
-class fila {
-    int N;
-    int erro;
-    int elements[MAX];
-    int primeiro;
-    int ultimo;
+class Queue {
+    public:
+        int N;
+        int erro;
+        Node *ptr_top;
+        Node *ptr_lest;
 
-    void criar() {
-        N = 0;
-        primeiro = 0;
-        ultimo = 0;
-    }
-
-    bool isEmpty() {
-        return N==0;
-    }
-
-    void enqueue(int dado) {
-        erro = 0;
-        if(N<MAX) {
-            elements[ultimo] = dado;
-            ultimo++;
-            N++;
-            if(ultimo == MAX) {
-                ultimo = 0;
-            }
-        }else {
-            erro = 1;
-            return;
+        void initilizate() {
+            this->N = 0;
+            this->ptr_top = nullptr;
+            this->ptr_lest = nullptr;
+            this->erro = 0;
         }
-    }
 
-    int dequeue() {
-        erro = 0;
-        if(!isEmpty()) {
-            int aux = elements[primeiro];
-            primeiro++;
-            N--;
-            return aux;
-        }else {
+        bool IsEmpty() {
+            return this->N == 0;
+        }
+
+        void Enqueue(int data) {
+            Node *newData = new Node();
+            newData->value = data;
+            newData->next = nullptr;
+
+            if(IsEmpty()) {
+                ptr_top = newData;
+                ptr_lest = newData;
+            }
+
+            ptr_lest->next = newData;
+            ptr_lest = newData;
+            N++;
+        }
+
+        int Dequeue() {
+            erro = 0;
+            if(!IsEmpty()) {
+                Node *temp = ptr_top;
+                int aux = temp->value;
+                ptr_top = ptr_top->next;
+                delete temp;
+                return aux;
+                N--;
+            }
+
             erro = 1;
             return -1;
         }
-    }
 
-    int front() {
-        return elements[primeiro];
-    }
+        int top() {
+            return this->ptr_top->value;
+        }
+
+        int Length() {
+            return this->N;
+        }
 };
+
+int main() {
+    Queue p;
+    p.initilizate();
+    p.Enqueue(1);
+    p.Enqueue(2);
+    p.Enqueue(3);
+
+    std::cout<<p.Length()<<std::endl;;
+    std::cout<<p.top()<<std::endl;;
+    std::cout<<p.Dequeue()<<std::endl;
+
+    p.Enqueue(6);
+    std::cout<<p.Length()<<std::endl;
+    std::cout<<p.Dequeue()<<std::endl;
+
+    return 0;
+}
