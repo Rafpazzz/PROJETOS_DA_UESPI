6e3e1aba6330e98f43676835956e4d82d474b0df
Rafael Paz <rafael_mourapaz@hotmail.com>
2025-03-12 17:27:08 -0300

    Create ArraysAleatorios.java

---
+++ PROGRAMAS_EM_JAVA/EXERCIOS_SALA/Exercico03p2/ArraysAleatorios.java
@@ -0,0 +1,11 @@
+package PROGRAMAS_EM_JAVA.EXERCIOS_SALA.Exercico03p2;
+
+public class ArraysAleatorios{
+    public static void main(String[] args) {
+        int array[] = {1,2,3,4,5};
+       
+    }
+
+    public static void ArrayVerifica(int arr[]) {
+    }
+}
