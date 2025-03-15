c01bad9f9a7bc5b7d39538b61f3d583163febc68
Rafael Paz <rafael_mourapaz@hotmail.com>
2025-03-14 22:36:24 -0300

    Listas adjacentes

---
+++ PROGRAMAS_EM_JAVA/EstruturaDeDados/ListaAdj.java
@@ -0,0 +1,14 @@
+package PROGRAMAS_EM_JAVA.EstruturaDeDados;
+
+import java.util.*;
+
+public class ListasAdjacentes {
+    public static void main(String[] args) {
+        
+    }
+
+}
+
+public class ListaAdj {
+    public
+}
