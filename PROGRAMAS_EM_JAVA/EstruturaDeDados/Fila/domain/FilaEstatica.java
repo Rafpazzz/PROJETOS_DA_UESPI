2eb87470a0af2148b0db6f1d0ecfec7f485982ab
Rafael Paz <rafael_mourapaz@hotmail.com>
2025-03-25 20:49:48 -0300

    estudo de filas estaticas e de prioridade

--- PROGRAMAS_EM_JAVA/EstruturaDeDados/Fila/domain/FilaEstatica.java
+++ PROGRAMAS_EM_JAVA/EstruturaDeDados/Fila/domain/FilaEstatica.java
@@ -1,8 +1,8 @@
 package PROGRAMAS_EM_JAVA.EstruturaDeDados.Fila.domain;
 
 public class FilaEstatica<T> {
-    private T[] a;
-    private int cont;
+    protected T[] a;
+    protected int cont;
     private int tamanho;
 
     public FilaEstatica(int tamanho) {
@@ -19,6 +19,20 @@ public class FilaEstatica<T> {
             System.out.println("A Fila esta lotada.");
             e.printStackTrace();
         }
+    }
+
+    public void adicionaDeterminadaPosicao(T data, int posicao) throws Exception {
+        if(posicao > tamanho && cont == tamanho && posicao<0) {
+            throw new IllegalArgumentException("Posição invalida, ou não ha espaço no array");
+        }
+
+        for(int i = cont; i > posicao; i--) {
+            a[i] = a[i-1];
+        }
+
+        a[posicao] = data;
+        cont++;
+
     }
 
     public void aumentaCapaciadade(int n) {
@@ -39,16 +53,38 @@ public class FilaEstatica<T> {
     }
 
     public T desenfileirar() {
-           T temp = a[0];
-           for(int i = 0; i<cont; i++) {
-               a[i] = a[i+1];
-           }
-           cont--;
-            return temp;
-       }
+        if(isEmpty()) {
+            return null;
+        }
+        T temp = a[0];
+        for(int i = 0; i<cont; i++) {
+            a[i] = a[i+1];
+        }
+        cont--;
+        return temp;
+    }
 
     public int tamanho() {
         return cont;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+
+        for(int i = 0; i<cont-1; i++) {
+            s.append(a[i]);
+            s.append(", ");
+        }
+
+        if(cont>0) {
+            s.append(a[cont-1]);
+        }
+
+        s.append("]");
+
+        return s.toString();
     }
 }
 
